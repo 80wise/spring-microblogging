7e2cb310f8a840eab4dbb21010a75726b076819f
80wise <waterkalubi@gmail.com>
2022-12-23 02:05:53 +0300

    real commit

--- src/main/kotlin/com/godsonpeya/microblog/controller/PostController.kt
+++ src/main/kotlin/com/godsonpeya/microblog/controller/PostController.kt
@@ -1,4 +1,32 @@
 package com.godsonpeya.microblog.controller
 
-class PostController {
+import com.godsonpeya.microblog.entity.Post
+import com.godsonpeya.microblog.service.PostService
+import com.godsonpeya.microblog.service.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+
+class PostController @Autowired constructor(private val postService: PostService) {
+    @GetMapping("/posts")
+    fun getPostList(): List<Post> = postService.getPosts()
+
+    @GetMapping("/posts/{id}")
+    fun getUser(@PathVariable id: Long): Post = postService.getOne(id)
+
+    @PostMapping("/posts")
+    fun createUser(@RequestBody post: Post): Post = postService.savePost(post)
+
+    @PutMapping("/posts/{id}")
+    fun updatePost(@PathVariable id: Long, @RequestBody post: Post): Post = postService.updatePost(id, post)
+
+    @DeleteMapping("/posts/{id}")
+    fun deleteUser(@PathVariable id: Long): String = postService.deletePost(id)
 }
--- src/main/kotlin/com/godsonpeya/microblog/service/PostService.kt
+++ src/main/kotlin/com/godsonpeya/microblog/service/PostService.kt
@@ -1,4 +1,31 @@
 package com.godsonpeya.microblog.service
 
-class PostService {
+import com.godsonpeya.microblog.entity.Post
+import com.godsonpeya.microblog.repository.PostRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class PostService @Autowired constructor(private var postRepository: PostRepository) {
+    fun getPosts(): List<Post> = postRepository.findAll()
+    fun getOne(id: Long): Post =
+        postRepository.findById(id).orElseThrow { IllegalArgumentException("Post with id = $id was not found") }
+
+    fun savePost(post: Post): Post = postRepository.save(post)
+    fun updatePost(id:Long, postInput: Post):Post{
+        val postFound = getOne(id)
+
+        postFound.content = postInput.content
+        postFound.userId = postInput.userId
+        postFound.createdAt = postInput.createdAt
+        postFound.updatedAt = postInput.updatedAt
+        return postRepository.save(postFound)
+    }
+    fun deletePost(id:Long): String {
+        val postFound = getOne(id)
+
+        postRepository.delete(postFound)
+
+        return "post deleted"
+    }
 }
